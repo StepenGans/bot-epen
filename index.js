bd541dbfbdaa25c113d9e1be57055830af25d756
StepenGans <74128777+StepenGans@users.noreply.github.com>
2021-01-05 23:21:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
 const { color, bgcolor } = require('./lib/color')
 const { help } = require('./src/help')
 const { info } = require('./src/info')
-const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
+const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, success, close } = require('./lib/functions')
 const { fetchJson } = require('./lib/fetcher')
 const { recognize } = require('./lib/ocr')
 const fs = require('fs')

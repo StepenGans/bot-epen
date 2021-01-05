71b36f0c0b3555ec2498ddc393898aefede3f116
StepenGans <74128777+StepenGans@users.noreply.github.com>
2021-01-05 21:14:13 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,176 @@
+const help = (prefix) => {
+	return `*⧨▣══⟪⊷͜͡҉𒊮᷍𝒁⎼𝒑𝑳𝒆𝒓𝒙᷍𒊮͜͡⊶҉⟫══▣⧨*
+*𝑯𝑨𝑳𝑶🖐️~*
+
+
+Gunakan command *_donasi* untuk berdonasi kepada author bot💰
+
+Gunakan command *_info* untuk melihat author bot ⚙️
+
+
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+║ 𝑭𝑬𝑨𝑻𝑼𝑹𝑬𝑺 𝑨𝑽𝑨𝑰𝑳𝑨𝑩𝑳𝑬 ║▣͜͡➢Prefix = "_"
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+
+╔══[ 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 ]══╗
+╠▣͜͡➢ *${prefix}sticker*
+╠▣͜͡➢ *${prefix}tsticker*
+╚════════════╝
+
+╔══[ 𝗠𝗢𝗥𝗘 ]══╗
+╠▣͜͡➢ *${prefix}donasi*
+║▣͜͡➢ *${prefix}info*
+╚══════════╝
+
+╔══[ 𝗠𝗔𝗞𝗘𝗥 ]══╗
+╠▣͜͡➢ *${prefix}nulis <teks>*
+╠▣͜͡➢ *${prefix}text3d <teks>*
+╠▣͜͡➢ *${prefix}ninjalogo <teks>*
+╠▣͜͡➢ *${prefix}wolflogo <teks>*
+╠▣͜͡➢ *${prefix}lionlogo <teks>*
+╠▣͜͡➢ *${prefix}textscreen <teks>*
+╠▣͜͡➢ *${prefix}tahta <teks>*
+╠▣͜͡➢ *${prefix}rtext <teks>*
+╠▣͜͡➢ *${prefix}thunder <teks>*
+╠▣͜͡➢ *${prefix}glitch <teks | teks>*
+╠▣͜͡➢ *${prefix}party <teks>*
+╠▣͜͡➢ *${prefix}lovemake <teks>*
+╠▣͜͡➢ *${prefix}galaxtext <teks>*
+╠▣͜͡➢ *${prefix}pronlogo <teks>*
+╠▣͜͡➢ *${prefix}stiltext <teks>*
+║▣͜͡➢ *${prefix}quotemaker <teks>*
+╚═══════════╝
+
+╔══[ 𝗙𝗨𝗡 𝗖𝗠𝗗 ]══╗
+╠▣͜͡➢ *${prefix}quotes*
+╠▣͜͡➢ *${prefix}lirik*
+╠▣͜͡➢ *${prefix}bucin*
+╠▣͜͡➢ *${prefix}tebakgambar*
+╠▣͜͡➢ *${prefix}caklontong*
+╠▣͜͡➢ *${prefix}family100*
+╠▣͜͡➢ *${prefix}game*
+╠▣͜͡➢ *${prefix}primbonjodoh <nama>|<nama>*
+╠▣͜͡➢ *${prefix}artinama <nama>*
+╠▣͜͡➢ *${prefix}ramalhp <nomor>*
+╠▣͜͡➢ *${prefix}ytsearch*
+╠▣͜͡➢ *${prefix}ssweb*
+╠▣͜͡➢ *${prefix}shorturl*
+╠▣͜͡➢ *${prefix}map*
+╠▣͜͡➢ *${prefix}ceckjodoh*
+╠▣͜͡➢ *${prefix}walpaperhd*
+╠▣͜͡➢ *${prefix}toimg*
+╠▣͜͡➢ *${prefix}report*
+╠▣͜͡➢ *${prefix}meme*
+╠▣͜͡➢ *${prefix}memeindo*
+╠▣͜͡➢ *${prefix}simi*
+╠▣͜͡➢ *${prefix}apakah*
+╠▣͜͡➢ *${prefix}bisakah*
+╠▣͜͡➢ *${prefix}kapankah*
+╠▣͜͡➢ *${prefix}watak*
+╠▣͜͡➢ *${prefix}mlherolist*
+╠▣͜͡➢ *${prefix}fototiktok*
+╠▣͜͡➢ *${prefix}playstore*
+╠▣͜͡➢ *${prefix}kbbi*
+╠▣͜͡➢ *${prefix}say*
+╠▣͜͡➢ *${prefix}wiki*
+╠▣͜͡➢ *${prefix}truth*
+╠▣͜͡➢ *${prefix}dare*
+╠▣͜͡➢ *${prefix}rate*
+╠▣͜͡➢ *${prefix}inu*
+╠▣͜͡➢ *${prefix}fitnah*
+╠▣͜͡➢ *${prefix}hilih*
+╠▣͜͡➢ *${prefix}speed*
+╠▣͜͡➢ *${prefix}brainly*
+╠▣͜͡➢ *${prefix}image*
+╠▣͜͡➢ *${prefix}pinterest*
+╠▣͜͡➢ *${prefix}indohot*
+╠▣͜͡➢ *${prefix}randomcat*
+╠▣͜͡➢ *${prefix}joox*
+╠▣͜͡➢ *${prefix}tts <teks>*
+║▣͜͡➢ *${prefix}url2img*
+╚════════════╝
+
+╔══[ 𝗡𝗦𝗙𝗪 ]══╗
+╠▣͜͡➢ *${prefix}randomhentai*
+╠▣͜͡➢ *${prefix}nsfwtrap*
+╠▣͜͡➢ *${prefix}nsfwloli*
+╠▣͜͡➢ *${prefix}nsfwneko*
+║▣͜͡➢ *${prefix}nsfwblowjob*
+╚══════════╝
+
+╔══[ 𝗔𝗡𝗜𝗠𝗔𝗟𝗦 ]══╗
+╠▣͜͡➢ *${prefix}elang*
+╠▣͜͡➢ *${prefix}babi*
+╠▣͜͡➢ *${prefix}unta*
+╠▣͜͡➢ *${prefix}anjing*
+║▣͜͡➢ *${prefix}pokemon*
+╚════════════╝
+
+╔══[ 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 ]══╗
+╠▣͜͡➢ *${prefix}ytsearch* [search yt]
+╠▣͜͡➢ *${prefix}ytmp3* [link]
+╠▣͜͡➢ *${prefix}ytmp4* [link]
+║▣͜͡➢ *${prefix}tiktok* [link] 
+╚══════════════╝
+
+╔══[ 𝗚𝗥𝗢𝗨𝗕 ]══╗
+╠▣͜͡➢ *${prefix}add* [62xxx]*
+╠▣͜͡➢ *${prefix}kick* [tag]
+╠▣͜͡➢ *${prefix}setpp*
+╠▣͜͡➢ *${prefix}setname*
+╠▣͜͡➢ *${prefix}setdesc*
+╠▣͜͡➢ *${prefix}demote* [tag]
+╠▣͜͡➢ *${prefix}promote* [tag]
+╠▣͜͡➢ *${prefix}tagall*
+╠▣͜͡➢ *${prefix}group* [buka/tutup]
+╠▣͜͡➢ *${prefix}welcome* [1/0]
+╠▣͜͡➢ *${prefix}nsfw* [1/0]
+╠▣͜͡➢ *${prefix}simih* [1/0]
+╠▣͜͡➢ *${prefix}listadmin*
+╠▣͜͡➢ *${prefix}groupinfo
+╠▣͜͡➢ *${prefix}hidetag*
+╠▣͜͡➢ *${prefix}tagme*
+║▣͜͡➢ *${prefix}delete*
+╚═══════════╝
+
+╔══[ 𝗜𝗡𝗙𝗢 ]══╗
+╠▣͜͡➢ *${prefix}infonomor*
+╠▣͜͡➢ *${prefix}infomotor*
+╠▣͜͡➢ *${prefix}infomobil*
+╠▣͜͡➢ *${prefix}blocklist*
+╠▣͜͡➢ *${prefix}infogempa*
+╠▣͜͡➢ *${prefix}infogithub*
+╠▣͜͡➢ *${prefix}infocuaca*
+╠▣͜͡➢ *${prefix}resepmasakan*
+║▣͜͡➢ *${prefix}creator*
+╚═════════╝
+
+╔══[ 𝗢𝗪𝗡𝗘𝗥 ]══╗
+╠▣͜͡➢ *${prefix}bc* 
+╠▣͜͡➢ *${prefix}leave*
+╠▣͜͡➢ *${prefix}clearall*
+╠▣͜͡➢ *${prefix}setprefix*
+╠▣͜͡➢ *${prefix}clone* [tag]
+╠▣͜͡➢ *${prefix}block*
+╠▣͜͡➢ *${prefix}unblock*
+║▣͜͡➢ *${prefix}getses*
+╚══════════╝
+
+╔══[ 𝗪𝗜𝗕𝗨 ]══╗
+╠▣͜͡➢ *${prefix}nekonime*
+╠▣͜͡➢ *${prefix}randomanime*
+╠▣͜͡➢ *${prefix}wait*
+╠▣͜͡➢ *${prefix}anime*
+╠▣͜͡➢ *${prefix}loli*
+║▣͜͡➢ *${prefix}waifu*
+╚═════════╝
+
+╔══[ 𝗦𝗧𝗔𝗟𝗞 ]══╗
+╠▣͜͡➢ *${prefix}tiktokstalk* 
+╚══════════╝
+
+*Jika ada bug pada bot silahkan hubungi admin di __info_*
+`
+}
+
+exports.help = help
---
+++ src/info.js
@@ -0,0 +1,47 @@
+const info = (prefix) => {
+	return `>⚞☞𝙸𝙽𝙵𝙾 𝙱𝙾𝚃☜⚟
+
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+
+⊷➣𝙰𝚞𝚝𝚑𝚘𝚛: 𝕰𝕺𝖃 - StevenTs
+⊷➣𝙶𝚒𝚝𝚑𝚞𝚋: https://www.github.com/StepenGans/
+
+⊷➣𝙿𝚊𝚛𝚝𝚗𝚎𝚛:𝕰𝕺𝖃 - hmzxy_
+⊷➣𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖: https://instagram.com/hnzxy_
+
+𝙆𝙖𝙡𝙖𝙪 𝙈𝙖𝙪 𝘿𝙤𝙣𝙖𝙨𝙞 𝙎𝙞𝙡𝙖𝙝𝙠𝙖𝙣 𝙆𝙚𝙩𝙞𝙠 _𝙙𝙤𝙣𝙖𝙨𝙞
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+
+𝙁𝙤𝙡𝙡𝙤𝙬 𝘼𝙪𝙩𝙝𝙤𝙧 𝘽𝙤𝙩
+
+⊷➣𝚈𝚘𝚞𝚝𝚞𝚋𝚎: https://youtube.com/c/StevenTs-EDX
+⊷➣𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙: wa.me/62815150192842
+⊷➣𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖: https://instagram.com/extoffice_
+⊷➣𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔: Steven Tri Saputra
+
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+
+INFO LAIN: Bot Ini Dikembangkan Oleh Steven, Bot Ini Menggunakan Bahasa Pemprograman Json(JavaScript) Dan Saya Menambahkan Fitur Fitur Baru
+
+Original Script By : Mhankbarbar
+
+*Bot On : Setiap Hari (24jam)
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+Note : Kalo Bot Off Berarti Bot Maintenance
+⟣⊷⊷⊷⊷᪣⟪▣⟫᪣⊶⊶⊶⊶⟢
+*Special Thanks To :*
+-Allah
+-Nabi Muhammad
+-Ayah Ibu
+-Zxbin/Galang
+-ArmanTod
+-Fdciabdul
+-St4rz(Bintang Nur Pradama) 
+-Mhankbarbar
+-Arugaz
+-Reuzsc
+`
+}
+
+exports.info = info
+
